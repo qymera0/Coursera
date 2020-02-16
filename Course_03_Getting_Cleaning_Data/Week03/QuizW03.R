723b17d348109d9733049970caf5e3a83389e2c5
Samuel B Baco <samuelbaco@gmail.com>
2020-02-16 12:55:20 -0300

    quiz t2

--- Course_03_Getting_Cleaning_Data/Week03/QuizW03.R
+++ Course_03_Getting_Cleaning_Data/Week03/QuizW03.R
@@ -12,3 +12,31 @@ idaho <- read.csv("idaho.csv", header = TRUE)
 
 agricultureLogical <- with(idaho, ACR == 10 & AGS == 6)
         
+
+# QUESTION 03 -------------------------------------------------------------
+
+link2 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+
+download.file(link2, destfile = "gross.csv")
+
+gross <- read.csv("gross.csv", header = FALSE, skip = 5, nrows = 190)
+
+gross$V6 <- as.numeric(gsub(",", "", gross$V5))
+
+link3 <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+
+download.file(link3, destfile = "edu.csv")
+
+edu <- read.csv("edu.csv")
+
+df <- merge(gross, edu, by.y = "CountryCode", by.x = "V1")
+
+
+
+# QUESTION 04 -------------------------------------------------------------
+
+tapply(df$V2, df$Income.Group, mean, na.rm = TRUE)
+
+# QUESTION 05 -------------------------------------------------------------
+
+
