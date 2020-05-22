c0cdccedff7afd42c102caa9c1462e67c57d07e1
Samuel B Baco <samuelbaco@gmail.com>
2020-05-22 15:04:52 -0300

    03

--- Course_08_Machine_Learning/Week04/Course Project/HAR_Auxiliar_Script.R
+++ Course_08_Machine_Learning/Week04/Course Project/HAR_Auxiliar_Script.R
@@ -1,3 +1,13 @@
+
+# 0. LOAD PACKAGES --------------------------------------------------------
+
+library(tidyverse)
+library(caret)
+library(future)
+library(doParallel)
+
+# 1. DATA DOWNLOAD --------------------------------------------------------
+
 fileLinkTraining <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv" 
 
 fileLinkTest <- "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
@@ -6,4 +16,20 @@ harTraining <- read.csv(fileLinkTraining)
 
 harTest <- read.csv(fileLinkTest)
 
-str(harTest)
+# 2. DATA WRANGLING -------------------------------------------------------
+
+not_all_na <- function(x) any(!is.na(x))
+
+harTstClean <-
+        harTest %>%
+        select(-c("X", 
+                  "user_name", 
+                  "raw_timestamp_part_1", 
+                  "raw_timestamp_part_2", 
+                  "cvtd_timestamp",
+                  "problem_id",
+                  "num_window")) %>%
+        select_if(not_all_na) %>%
+        select_if(~n_distinct(.) > 1)
+        
+harTrnClean <- harTraining[ ,c(names(harTstClean), "classe")]
