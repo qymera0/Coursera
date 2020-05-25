8acec831f6a35564c0e46d9597d96b63b7c967a0
Samuel Baco <samuelbaco@gmail.com>
2020-05-25 14:21:29 -0300

    logistc1

--- Course_08_Machine_Learning/Week04/Course Project/HAR_Auxiliar_Script.R
+++ Course_08_Machine_Learning/Week04/Course Project/HAR_Auxiliar_Script.R
@@ -1,8 +1,13 @@
 
 # 0. LOAD PACKAGES --------------------------------------------------------
-
 library(tidyverse)
 library(caret)
+library(future)
+library(doParallel)
+library(heatmaply)
+library(factoextra)
+library(FactoMineR)
+library(nnet)
 library(future)
 library(doParallel)
 
@@ -33,3 +38,48 @@ harTstClean <-
         select_if(~n_distinct(.) > 1)
         
 harTrnClean <- harTraining[ ,c(names(harTstClean), "classe")]
+
+
+# 3. EDA ------------------------------------------------------------------
+
+# 3.1 Null performance model
+
+nullPred <- test %>% select("classe")
+
+nullPred$pred.class <- names(sort(table(nullPred$classe), decreasing = TRUE)[1])
+
+print(confusionMatrix(as.factor(nullPred$pred.class), reference = as.factor(nullPred$classe)))
+
+# 3.2 Multicolinearity
+
+corrMat <- harTrnClean %>% select(-classe) %>% mutate_if(is.integer, as.numeric) %>% cor()
+
+heatmaply(corrMat, symm = TRUE, cexRow = .0001, cexCol = .0001, branches_lwd = .1)
+
+pcaCov <- harTrnClean %>% select(-classe) %>% PCA(scale.unit = TRUE, ncp = 10, graph = FALSE)
+
+get_eigenvalue(pcaCov)
+
+fviz_eig(pcaCov)
+
+# 4 MODELING --------------------------------------------------------------
+
+workers <- availableCores()
+
+cl <- makeClusterPSOCK(workers)
+
+registerDoParallel(cl)
+
+# 4.1 Multinomial Regression
+
+mdlLrOr <- train(classe ~., data = harTrnClean, method = 'multinom')
+
+stopCluster(cl)   
+
+summary(mdlLrOr)
+
+registerDoParallel(cl)
+
+mdlLrSc <- train(classe ~., data = harTrnClean, method = 'multinom', preProcess = "scale")
+
+stopCluster(cl)
