dff12e6be70e172432b907102bdef281d3929043
Samuel B Baco <samuelbaco@gmail.com>
2020-07-16 17:25:15 -0300

    Task 1 final

--- Course_10_Capstone/Week02/Aux_Script.R
+++ Course_10_Capstone/Week02/Aux_Script.R
@@ -12,3 +12,12 @@ future_map(stri_trans_general, id = "Latin-ASCII")
 as_tibble() %>%
         gather(key = "source") %>%
         mutate(source = as.factor(source))
+
+
+map(replace_contraction) %>%
+        map(replace_emoji) %>%
+        map(replace_hash) %>%
+        map(replace_internet_slang) %>%
+        map(replace_url) %>%
+        map(replace_non_ascii) %>%
+        map(replace_incomplete) %>%
